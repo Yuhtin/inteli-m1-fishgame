ba8d6d902dd59c63a174bbc28c5522a0c17582b9
Davi Duarte <rio.daviduarte@gmail.com>
2025-02-03 13:55:31 -0300

    feat: game logic w/ crazy ideas

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
+var debug = false; // Se o debug estiver ativo, irá desenhar um circulo vermelho com o raio configurado em maxDeltaRadius
+var maxDeltaRadius = 75; // Raio de ação do mouse
+var moveAround = false; // Habilita o movimento aleatório dos peixes
+var gameModifications = true; // Habilita as modificações no jogo
+
 var config = {
     type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
+    width: 800,
+    height: 600,
 
     scene: {
         preload: preload,
@@ -11,32 +16,86 @@ var config = {
 }
 
 var game = new Phaser.Game(config);
+var peixes = [];
 
 function preload() {
     this.load.image('mar', 'assets/bg_azul-escuro.png');
-    this.load.image('logo' , 'assets/logo-inteli_branco.png');
-   
-    var files = fs.readdirSync('/assets/peixes/');
+    this.load.image('logo', 'assets/logo-inteli_branco.png');
 
-    files.forEach(file => {
-        this.load.image('peixe' + i, '/assets/peixes/' + file);
-    });
+    this.load.image('peixe0', 'assets/peixes/baiacu_lado.png');
+    this.load.image('peixe1', 'assets/peixes/baiacu.png');
+    this.load.image('peixe2', 'assets/peixes/peixinho_laranja.png');
+    this.load.image('peixe3', 'assets/peixes/tartaruga.png');
 }
 
 function create() {
-    var files = fs.readdirSync('/assets/peixes/');
-    
-    for (var i = 0; i < files.length; i++) {
-        // put the image in the scene in a random position
-        var x = Phaser.Math.Between(0, 1920);
-        var y = Phaser.Math.Between(0, 1080);
+    this.add.image(400, 300, 'mar');
 
-        this.add.image(x, y, 'peixe' + i).setOrigin(0.5, 0.5).setFlip(true, false);
+    var maxRange = 3;
+
+    if (gameModifications) {
+        for (var i = 0; i < 25; i++) {
+            // put the image in the scene in a random position
+            var x = Phaser.Math.Between(0, 960);
+            var y = Phaser.Math.Between(0, 540);
+
+            var number = Phaser.Math.Between(0, maxRange);
+            peixes.push(this.add.image(x, y, 'peixe' + number).setScale(0.5).setOrigin(0.5, 0.5).setFlip(true, false));
+        }
+    } else {
+        peixes.push(this.add.image(400, 300, 'peixe0').setOrigin(0.5, 0.5).setFlip(true, false));
     }
 
-    this.add.image(400, 525, 'logo').setScale(0.5);
+    this.add.image(400, 525, 'logo').setScale(0.7);
 }
 
 function update() {
+    var currentX = this.input.x;
+    var currentY = this.input.y;
 
+    if (!gameModifications) {
+        peixes[0].x = currentX;
+        peixes[0].y = currentY;
+
+        return;
+    }
+
+    for (var i = 0; i < peixes.length; i++) {
+        var deltaX = currentX - peixes[i].x;
+        var deltaY = currentY - peixes[i].y;
+
+        var deltaRadius = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        if (deltaRadius < maxDeltaRadius) {
+            // Mover o peixe para longe do mouse
+            var x = peixes[i].x - deltaX / 5;
+            var y = peixes[i].y - deltaY / 5;
+
+            // Mover o peixe para o outro lado da tela caso ele saia
+            peixes[i].x = Phaser.Math.Wrap(x, 50, 750);
+            peixes[i].y = Phaser.Math.Wrap(y, 50, 550);
+
+            // Inverter a imagem do peixe de acordo com a direção do mouse
+            peixes[i].setFlip(deltaX < 0, false);
+        } else {
+            if (moveAround) {
+                if (Phaser.Math.Between(0, 5) == 0) {
+                    var x = peixes[i].x - Phaser.Math.Between(0, 5);
+                    peixes[i].x = Phaser.Math.Wrap(x, 50, 750);
+                } else {
+                    var y = peixes[i].y - Phaser.Math.Between(0, 5);
+                    peixes[i].y = Phaser.Math.Wrap(y, 50, 550);
+                }
+            }
+        }
+    }
+
+    if (debug) {
+        if (!this.graphics) {
+            this.graphics = this.add.graphics();
+        }
+
+        this.graphics.clear();
+        this.graphics.lineStyle(2, 0xff0000);
+        this.graphics.strokeCircle(currentX, currentY, maxDeltaRadius);
+    }
 }
