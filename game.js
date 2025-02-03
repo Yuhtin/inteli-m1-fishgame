617f40a43793c87a9e28298a1e512a6d482446b6
Davi Duarte <rio.daviduarte@gmail.com>
2025-02-03 12:45:35 -0300

    feat: phaser.js

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-var fs = require('fs');
-
 var config = {
     type: Phaser.AUTO,
     width: 1920,
@@ -16,12 +14,27 @@ var game = new Phaser.Game(config);
 
 function preload() {
     this.load.image('mar', 'assets/bg_azul-escuro.png');
-    
+    this.load.image('logo' , 'assets/logo-inteli_branco.png');
+   
+    var files = fs.readdirSync('/assets/peixes/');
+
+    files.forEach(file => {
+        this.load.image('peixe' + i, '/assets/peixes/' + file);
+    });
 }
 
 function create() {
-    this.add.image(960, 540, 'fundo');
-    this.add.image(960, 540, 'peixe');
+    var files = fs.readdirSync('/assets/peixes/');
+    
+    for (var i = 0; i < files.length; i++) {
+        // put the image in the scene in a random position
+        var x = Phaser.Math.Between(0, 1920);
+        var y = Phaser.Math.Between(0, 1080);
+
+        this.add.image(x, y, 'peixe' + i).setOrigin(0.5, 0.5).setFlip(true, false);
+    }
+
+    this.add.image(400, 525, 'logo').setScale(0.5);
 }
 
 function update() {
